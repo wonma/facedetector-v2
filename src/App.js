e573282e30e66fe6fec865d45a669b96000ac043
Wonmi <wonmadesign@gmail.com>
2023-12-19 11:31:02 -0600

    Fixed the Clarifai API call

--- src/App.js
+++ src/App.js
@@ -81,46 +81,97 @@ class App extends React.Component {
       boxScore: { active: '' }
     });
     const { totalScore, totalCredit } = this.state.user;
-    app.models
-      .predict('a403429f2ddf4b49b307e318f00e528b', this.state.input)
-      .then(response => {
-        const dataArray = response.outputs[0].data.regions;
-        this.updateBoxState(this.calculateBox(dataArray));
-        this.setState({
-          boxScore: {
-            active: 'active',
-            score: dataArray.length * 10
-          }
-        });
 
-        fetch('http://localhost:5000/image', {
-          method: 'put',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            id: this.state.user.id,
-            totalScore: totalScore + dataArray.length * 10
-          })
-        })
-          .then(response => response.json())
-          .then(({ totalScore, totalCredit }) => {
-            this.setState({
-              user: Object.assign(this.state.user, {
-                totalScore: totalScore,
-                totalCredit: totalCredit
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    // In this section, we set the user authentication, user and app ID, model details, and the URL
+    // of the image we want as an input. Change these strings to run your own example.
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = 'd00fe3e62c4e4c97b0383c75ab3bcf32';
+    const USER_ID = 'clarifai';
+    const APP_ID = 'main';
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+    const IMAGE_URL = this.state.input;
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                        // "base64": IMAGE_BYTES_STRING
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+ 
+   // Step1. Make API call to Clarifai server.
+   // - Step1-1. User fetch to send this.state.input(the url of the image).
+
+   fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+   .then(response => response.json())
+   .then(result => {
+        // Step1-2. Receive the response, render the box regions, and update the box score. 
+       const regions = result.outputs[0].data.regions;
+       this.updateBoxState(this.calculateBox(regions));
+       this.setState({
+         boxScore: {
+           active: 'active',
+           score: regions.length * 10
+         }
+       });
+       
+               // Step 2. Update Total Score and Total Credit.
+               fetch('http://localhost:5000/image', {
+                method: 'put',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                  id: this.state.user.id,
+                  totalScore: totalScore + regions.length * 10
+                })
               })
-            });
-            setTimeout(() => {
-              document.querySelector('.scoreAlarm').classList.remove('active');
-            }, 1000);
-          })
-          .catch(err => console.log(err));
-      })
-      .catch(err => {
-        console.log(err);
-      });
+              .then(response => response.json())
+              .then(({ totalScore, totalCredit }) => {
+                this.setState({
+                  user: Object.assign(this.state.user, {
+                    totalScore: totalScore,
+                    totalCredit: totalCredit
+                  })
+                });
+                setTimeout(() => {
+                  document.querySelector('.scoreAlarm').classList.remove('active');
+                }, 1000);
+              })
+              .catch(err => console.log(err));
+              // This is the end of 'Step 2'.
+   })
+   .catch(error => console.log('error', error));
+
   };
+
+
 
   onEnter = e => {
     if (e.keyCode == 13) {
